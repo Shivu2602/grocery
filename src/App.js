a45e9938c91b5f3e1bf2a84f6e1e47ddc36e4d52
Anandrao Malkari <107610914+anandraomalkari@users.noreply.github.com>
2023-10-12 19:56:38 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ProductCart from './component/homepage/product/productdetails/ProductCart
 import { Routes, Route } from "react-router-dom";
 import Cart from './component/cart/Cart';
 import ContactUs from './component/contactus/ContactUs';
+import DryFruitsDesc from './component/homepage/product/productlist/dryfruits/DryFruitsDesc';
+
 function App() {
   return (
     <>
@@ -18,6 +20,7 @@ function App() {
         <Route path="/getproductone/:id" element={<ProductCart />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/dryfruits" element={<DryFruitsDesc />} />
         /
         {/* <Route path="/employee" element={<EmployeeDirect />} /> */}
         {/* if user can click on any praticular product that product valid "id" available then that particular product detail show*/}
